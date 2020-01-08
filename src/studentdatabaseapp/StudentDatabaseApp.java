da651ee8e4f318ae7e5646a00ae21ddcba2b444f
Grzegorz Kozlowski <grz.kozlowski@gmail.com>
2020-01-08 17:14:54 +0100

    initial commit

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		// Ask how many users to add
+		
+		// Create n number of students
+
+	}
+
+}

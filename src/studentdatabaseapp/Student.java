bb4b3dc78defde78869c2c787f003021f892f99a
Grzegorz Kozlowski <grz.kozlowski@gmail.com>
2020-01-08 17:35:36 +0100

    change Student.java
    add setStudentID method

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -10,7 +10,9 @@ public class Student {
 	private int studentID;
 	private String courses;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	
+	private static int studentNo = 0;
 	
 	// Constructor: prompt to enter name and year
 	public Student() {
@@ -24,11 +26,16 @@ public class Student {
 		System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level");
 		this.gradeYear = in.nextInt();
 		
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		setStudentID();
 		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " ID:" + studentID);
 	}
 	
 	// Generate an ID
+	private void setStudentID() {
+		studentNo++;
+		this.studentID = gradeYear * 10000 + studentNo;
+	}
 	
 	// Enroll in courses
 	

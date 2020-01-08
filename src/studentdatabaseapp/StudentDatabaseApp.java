b0ed3cdb4a5e4d38845328aa4f0e117bdb23fb77
Grzegorz Kozlowski <grz.kozlowski@gmail.com>
2020-01-08 17:24:38 +0100

    change Student.java StudentDatabaseApp.java

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
+		Student stu1 = new Student();
 		// Ask how many users to add
 		
 		// Create n number of students
